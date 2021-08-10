47b1f28e61eea2c57ac2a7c34d0902482af13173
kyawmt <kmt@kmtkko.com>
2021-08-10 19:04:52 +0800

    Generated starter files for project

---
+++ app/src/main/java/sa52/team03/adproject/MainActivity.java
@@ -0,0 +1,14 @@
+package sa52.team03.adproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

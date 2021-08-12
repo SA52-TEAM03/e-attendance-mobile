75d6f36a3dd04d00f517408562bc5b0b3fec2719
kyawmt <kmt@kmtkko.com>
2021-08-12 12:09:32 +0800

    Add QRCodeActivity and FaceAuthentication Activity

---
+++ app/src/main/java/sa52/team03/adproject/QRCodeActivity.java
@@ -0,0 +1,14 @@
+package sa52.team03.adproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QRCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode);
+    }
+}
